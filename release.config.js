6941d78c56306a5d188a0aad2fbb4bc8d782c5a2
Rıdvan Altun <ridvanaltun@outlook.com>
2020-06-01 17:35:50 +0300

    ci: generate documentation during ci process

--- release.config.js
+++ release.config.js
@@ -3,9 +3,6 @@ module.exports = {
   'plugins': [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/exec', {
-      'generateNotesCmd': 'npm run doc',
-    }],
     ['@semantic-release/changelog', {
       'changelogFile': 'CHANGELOG.md',
     }],
