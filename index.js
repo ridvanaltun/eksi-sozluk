27bef2ed75d62d5e7d78fdd0796a95ebc27cd8d4
Rıdvan Altun <ridvanaltun@outlook.com>
2020-06-02 00:44:53 +0300

    feat: add ability to batch pull entry from a title

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const getEntryById = require('./lib/getEntryById');
+const getEntries = require('./lib/getEntries');
 const getUser = require('./lib/getUser');
 const getDebe = require('./lib/getDebe');
 
 const eksisozluk = {
   getEntryById,
+  getEntries,
   getUser,
   getDebe,
 };
