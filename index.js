2292274f27e0f0dc5a88ab6b5796474eac551f44
Rıdvan Altun <ridvanaltun@outlook.com>
2020-06-01 12:48:52 +0300

    feat: fetch debe

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const getEntryById = require('./lib/getEntryById');
 const getUser = require('./lib/getUser');
+const getDebe = require('./lib/getDebe');
 
 const eksisozluk = {
   getEntryById,
-  getUser
+  getUser,
+  getDebe
 };
 
 module.exports = eksisozluk;
