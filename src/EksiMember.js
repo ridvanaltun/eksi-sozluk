c6b04229958bbeef9c3851f3c901278fc430cca4
Rıdvan Altun <ridvanaltun@outlook.com>
2020-11-05 11:38:47 +0300

    feat(member): ability select page when fetch today, drafts and rookie entries

--- src/EksiMember.js
+++ src/EksiMember.js
@@ -27,33 +27,61 @@ class EksiMember extends EksiGeneral {
   /**
    * Fetch today.
    *
-   * @return {Today} A promise for the today.
+   * @param   {Object}  options           Parameters that user can specify.
+   * @param   {number}  [options.page=1]  Page.
+   * @return  {Today}   A promise for the today.
    */
-  today () {
+  today (options) {
+    // handle default options
+    const _options = objectAssignDeep(
+      {
+        page: 1
+      },
+      options
+    )
+
+    // handle params
+    const params = {
+      p: _options.page
+    }
+
     return new Promise((resolve, reject) => {
-      this._request({ endpoint: '/basliklar/bugun', ajax: true, cookie: this.cookies }, ($) => {
-        const status = $.statusCode
+      this._request(
+        {
+          endpoint: '/basliklar/bugun',
+          ajax: true,
+          cookie: this.cookies,
+          params
+        },
+        ($) => {
+          const status = $.statusCode
 
-        if (status === 200) {
-          const today = []
+          if (status === 200) {
+            const today = []
 
-          $('ul.topic-list.partial li').each(function (i, elm) {
-            const title = $(elm).text().trim()
-            const entryCount = $(elm).find('a small').text().trim()
-            today.push({
-              title: title.substring(0, title.length - (entryCount.length + 1)), // clear title
-              title_url: c.urls.base + $(elm).find('a').attr('href'),
-              entry_count: entryCount.includes('b')
-                ? 1000 * parseInt(entryCount)
-                : parseInt(entryCount) ? parseInt(entryCount) : 1 // calculate entry count
+            $('ul.topic-list.partial li').each(function (i, elm) {
+              const title = $(elm).text().trim()
+              const entryCount = $(elm).find('a small').text().trim()
+              today.push({
+                title: title.substring(
+                  0,
+                  title.length - (entryCount.length + 1)
+                ), // clear title
+                title_url: c.urls.base + $(elm).find('a').attr('href'),
+                entry_count: entryCount.includes('b')
+                  ? 1000 * parseInt(entryCount)
+                  : parseInt(entryCount)
+                    ? parseInt(entryCount)
+                    : 1 // calculate entry count
+              })
             })
-          })
 
-          resolve(today)
-        } else {
-          reject(new Error('An unknown error occurred.'))
+            resolve(today)
+          } else {
+            reject(new Error('An unknown error occurred.'))
+          }
         }
-      })
+      )
     })
   }
 
@@ -67,33 +95,61 @@ class EksiMember extends EksiGeneral {
   /**
    * Fetch rookie entries.
    *
-   * @return {RookieEntries} A promise for the rookie entries.
+   * @param   {Object}        options           Parameters that user can specify.
+   * @param   {number}        [options.page=1]  Page.
+   * @return  {RookieEntries} A promise for the rookie entries.
    */
-  rookieEntries () {
+  rookieEntries (options) {
+    // handle default options
+    const _options = objectAssignDeep(
+      {
+        page: 1
+      },
+      options
+    )
+
+    // handle params
+    const params = {
+      p: _options.page
+    }
+
     return new Promise((resolve, reject) => {
-      this._request({ endpoint: '/basliklar/caylaklar', ajax: true, cookie: this.cookies }, ($) => {
-        const status = $.statusCode
+      this._request(
+        {
+          endpoint: '/basliklar/caylaklar',
+          ajax: true,
+          cookie: this.cookies,
+          params
+        },
+        ($) => {
+          const status = $.statusCode
 
-        if (status === 200) {
-          const rookieEntries = []
+          if (status === 200) {
+            const rookieEntries = []
 
-          $('ul.topic-list.partial li').each(function (i, elm) {
-            const title = $(elm).text().trim()
-            const entryCount = $(elm).find('a small').text().trim()
-            rookieEntries.push({
-              title: title.substring(0, title.length - (entryCount.length + 1)), // clear title
-              title_url: c.urls.base + $(elm).find('a').attr('href'),
-              entry_count: entryCount.includes('b')
-                ? 1000 * parseInt(entryCount)
-                : parseInt(entryCount) ? parseInt(entryCount) : 1 // calculate entry count
+            $('ul.topic-list.partial li').each(function (i, elm) {
+              const title = $(elm).text().trim()
+              const entryCount = $(elm).find('a small').text().trim()
+              rookieEntries.push({
+                title: title.substring(
+                  0,
+                  title.length - (entryCount.length + 1)
+                ), // clear title
+                title_url: c.urls.base + $(elm).find('a').attr('href'),
+                entry_count: entryCount.includes('b')
+                  ? 1000 * parseInt(entryCount)
+                  : parseInt(entryCount)
+                    ? parseInt(entryCount)
+                    : 1 // calculate entry count
+              })
             })
-          })
 
-          resolve(rookieEntries)
-        } else {
-          reject(new Error('An unknown error occurred.'))
+            resolve(rookieEntries)
+          } else {
+            reject(new Error('An unknown error occurred.'))
+          }
         }
-      })
+      )
     })
   }
 
@@ -111,29 +167,36 @@ class EksiMember extends EksiGeneral {
    */
   events () {
     return new Promise((resolve, reject) => {
-      this._request({ endpoint: '/basliklar/olay', ajax: true, cookie: this.cookies }, ($) => {
-        const status = $.statusCode
+      this._request(
+        { endpoint: '/basliklar/olay', ajax: true, cookie: this.cookies },
+        ($) => {
+          const status = $.statusCode
 
-        if (status === 200) {
-          const events = []
+          if (status === 200) {
+            const events = []
 
-          $('ul.topic-list.partial li').each(function (i, elm) {
-            const title = $(elm).text().trim()
-            const entryCount = $(elm).find('a small').text().trim()
-            events.push({
-              title: title.substring(0, title.length - (entryCount.length)).trim(), // clear title
-              title_url: c.urls.base + $(elm).find('a').attr('href'),
-              entry_count: entryCount.includes('b')
-                ? 1000 * parseInt(entryCount)
-                : parseInt(entryCount) ? parseInt(entryCount) : 0 // calculate entry count
+            $('ul.topic-list.partial li').each(function (i, elm) {
+              const title = $(elm).text().trim()
+              const entryCount = $(elm).find('a small').text().trim()
+              events.push({
+                title: title
+                  .substring(0, title.length - entryCount.length)
+                  .trim(), // clear title
+                title_url: c.urls.base + $(elm).find('a').attr('href'),
+                entry_count: entryCount.includes('b')
+                  ? 1000 * parseInt(entryCount)
+                  : parseInt(entryCount)
+                    ? parseInt(entryCount)
+                    : 0 // calculate entry count
+              })
             })
-          })
 
-          resolve(events)
-        } else {
-          reject(new Error('An unknown error occurred.'))
+            resolve(events)
+          } else {
+            reject(new Error('An unknown error occurred.'))
+          }
         }
-      })
+      )
     })
   }
 
@@ -147,31 +210,54 @@ class EksiMember extends EksiGeneral {
   /**
    * Fetch drafts.
    *
-   * @return {Drafts} A promise for the drafts.
+   * @param   {Object}  options           Parameters that user can specify.
+   * @param   {number}  [options.page=1]  Page.
+   * @return  {Drafts}  A promise for the drafts.
    */
-  drafts () {
+  drafts (options) {
+    // handle default options
+    const _options = objectAssignDeep(
+      {
+        page: 1
+      },
+      options
+    )
+
+    // handle params
+    const params = {
+      p: _options.page
+    }
+
     return new Promise((resolve, reject) => {
-      this._request({ endpoint: '/basliklar/kenar', ajax: true, cookie: this.cookies }, ($) => {
-        const status = $.statusCode
+      this._request(
+        {
+          endpoint: '/basliklar/kenar',
+          ajax: true,
+          cookie: this.cookies,
+          params
+        },
+        ($) => {
+          const status = $.statusCode
 
-        if (status === 200) {
-          const drafts = []
+          if (status === 200) {
+            const drafts = []
 
-          $('ul.topic-list.partial li').each(function (i, elm) {
-            const title = $(elm).text().trim()
-            const date = $(elm).find('a div').text().trim()
-            drafts.push({
-              title: title.substring(0, title.length - (date.length)).trim(), // clear title
-              title_url: c.urls.base + $(elm).find('a').attr('href'),
-              draft_date: date
+            $('ul.topic-list.partial li').each(function (i, elm) {
+              const title = $(elm).text().trim()
+              const date = $(elm).find('a div').text().trim()
+              drafts.push({
+                title: title.substring(0, title.length - date.length).trim(), // clear title
+                title_url: c.urls.base + $(elm).find('a').attr('href'),
+                draft_date: date
+              })
             })
-          })
 
-          resolve(drafts)
-        } else {
-          reject(new Error('An unknown error occurred.'))
+            resolve(drafts)
+          } else {
+            reject(new Error('An unknown error occurred.'))
+          }
         }
-      })
+      )
     })
   }
 
@@ -191,9 +277,12 @@ class EksiMember extends EksiGeneral {
    */
   followedUserEntries (options) {
     // handle default options
-    const _options = objectAssignDeep({
-      page: 1
-    }, options)
+    const _options = objectAssignDeep(
+      {
+        page: 1
+      },
+      options
+    )
 
     // handle params
     const params = {
@@ -201,32 +290,35 @@ class EksiMember extends EksiGeneral {
     }
 
     return new Promise((resolve, reject) => {
-      this._request({
-        endpoint: '/basliklar/takipentry',
-        ajax: true,
-        cookie: this.cookies,
-        params
-      }, ($) => {
-        const status = $.statusCode
+      this._request(
+        {
+          endpoint: '/basliklar/takipentry',
+          ajax: true,
+          cookie: this.cookies,
+          params
+        },
+        ($) => {
+          const status = $.statusCode
 
-        if (status === 200) {
-          const followedUserEntries = []
+          if (status === 200) {
+            const followedUserEntries = []
 
-          $('ul.topic-list.partial li').each(function (i, elm) {
-            const title = $(elm).text().trim()
-            const owner = $(elm).find('a div').text().trim()
-            followedUserEntries.push({
-              title: title.substring(0, title.length - (owner.length)).trim(), // clear title
-              title_url: c.urls.base + $(elm).find('a').attr('href'),
-              entry_owner: owner
+            $('ul.topic-list.partial li').each(function (i, elm) {
+              const title = $(elm).text().trim()
+              const owner = $(elm).find('a div').text().trim()
+              followedUserEntries.push({
+                title: title.substring(0, title.length - owner.length).trim(), // clear title
+                title_url: c.urls.base + $(elm).find('a').attr('href'),
+                entry_owner: owner
+              })
             })
-          })
 
-          resolve(followedUserEntries)
-        } else {
-          reject(new Error('An unknown error occurred.'))
+            resolve(followedUserEntries)
+          } else {
+            reject(new Error('An unknown error occurred.'))
+          }
         }
-      })
+      )
     })
   }
 }
