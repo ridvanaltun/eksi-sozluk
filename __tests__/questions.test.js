f48e6aba141019a2e3eb2cf1ffad8f2512d58b14
Rıdvan Altun <ridvanaltun@outlook.com>
2020-11-16 17:14:21 +0300

    test: correct a test

--- __tests__/questions.test.js
+++ __tests__/questions.test.js
@@ -3,8 +3,15 @@ const { EksiSozluk } = require('../src/index')
 const instance = new EksiSozluk()
 
 describe('Questions Test', () => {
-  test('Getting Questions', () => {
-    return instance.questions()
+  test('Getting Questions in Agenda', () => {
+    return instance.questionsInAgenda()
+      .then((data) => {
+        // no test
+      })
+  })
+
+  test('Getting Questions in Today', () => {
+    return instance.questionsInToday()
       .then((data) => {
         // no test
       })
