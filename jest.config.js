7273ebdc13de2f82a129db441a1db9e4ee09fba3
Rıdvan Altun <ridvanaltun@outlook.com>
2021-09-01 01:06:12 +0300

    test: update test timeout 5 seconds to 10 seconds

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   roots: ['__tests__/'],
   // This line solves: Cross origin http://localhost forbidden
   // @see https://github.com/axios/axios/issues/1754
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  testTimeout: 10000 // default 5 secs, we are using 10 secs
 }
